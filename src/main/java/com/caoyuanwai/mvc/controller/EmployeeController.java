6d9f22f398d2d5292ab573686b81685479489c75
caoyuanwai <2553219190@qq.com>
2022-10-07 15:10:55 +0800

    springmvc

--- src/main/java/com/caoyuanwai/mvc/controller/EmployeeController.java
+++ src/main/java/com/caoyuanwai/mvc/controller/EmployeeController.java
@@ -1,11 +1,47 @@
 package com.caoyuanwai.mvc.controller;
 
+import com.caoyuanwai.mvc.bean.Employee;
 import com.caoyuanwai.mvc.dao.EmployeeDao;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
+    @GetMapping("/employee")
+    public String getAllEmployee(Model model) {
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList", employeeList);
+        return "employee_list";
+    }
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable("id")Integer id) {
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
+    @PostMapping("/employee")
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @GetMapping("/employee/{id}")
+    public String getEmployeeById(@PathVariable("id") Integer id,Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update.html";
+    }
+
+    @PutMapping("/employee")
+    public String updateEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
 }
